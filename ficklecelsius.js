c3011a5cb1cc38a55e99d554dcb2e807c06d86eb
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-04-22 16:40:10 -0400

    proper spelling

--- ficklecelsius.js
+++ ficklecelsius.js
@@ -1,29 +1,29 @@
 const sbeve = require('./sbeve.js')
 
-// you need formulas on how to convert from celcius to farenheit and vice versa
+// you need formulas on how to convert from celcius to fahrenheit and vice versa
 
 // (32°F − 32) × 5/9 = 0°C is F ---> C
 
 // (0°C × 9/5) + 32 = 32°F is C ---> F
 
 console.log("Well well well looks like someone forgot to go over the lab homework and came to me for help")
-console.log("Well then time to choose, dawg. Are you converting from Celsius to Farenheit or Farenheit to Celsius?")
-console.log("Choose 'farenheit' or 'celsius'")
+console.log("Well then time to choose, dawg. Are you converting from celsius to fahrenheit or fahrenheit to celsius?")
+console.log("Choose 'fahrenheit' or 'celsius'")
 
 var startingUnit
 
 startingUnit = sbeve.input()
 
-while (startingUnit !== 'farenheit' && startingUnit ==! 'celsius') {
+while (startingUnit !== 'fahrenheit' && startingUnit ==! 'celsius') {
     console.log('Wrong choice ya fool! Pick your initial unit of temperature!')
     startingUnit = sbeve.input()
 } 
 
-if (startingUnit === 'farenheit') {
-    console.log('Ok you chose farenheit. What is the temperature in °F?')
-    var farenheitTemp
-    farenheitTemp = sbeve.inputNumber()
+if (startingUnit === 'fahrenheit') {
+    console.log('Ok you chose fahrenheit. What is the temperature in °F?')
+    var fahrenheitTemp
+    fahrenheitTemp = sbeve.inputNumber()
 
-    celsiusConversion = (farenheitTemp - 32) * 5/9 
-    console.log(farenheitTemp,"°F converts to",celsiusConversion,"°C")
+    celsiusConversion = (fahrenheitTemp - 32) * 5/9 
+    console.log(fahrenheitTemp,"°F converts to",celsiusConversion,"°C")
 }
