a41f2ecbded7e15d9e288dff427862a60d768af7
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-04-22 18:57:05 -0400

    the same thing but replace 1 with 2

---
+++ countfranklinmode.js
@@ -0,0 +1,25 @@
+const sbeve = require('./sbeve.js')
+
+var startingCount = 2
+var yourCount
+
+console.log("Franklin the turtle can count by twos so can you!")
+
+yourCount = sbeve.inputNumber()
+
+if (yourCount !== startingCount ) {
+  console.log("Even Franklin knew you shoulda started at",startingCount+"! But you blew it!")
+} else {
+  var newCountToGuess = yourCount + 2
+
+  console.log("Keep counting! After you're done counting by twos we can tie some shoes!")
+  yourCount = sbeve.inputNumber()
+
+  while (yourCount === newCountToGuess) {
+    newCountToGuess = yourCount + 2
+    console.log("Good job! One step closing to tying those shoes!")
+    yourCount = sbeve.inputNumber()
+  }
+
+  console.log("You should have guessed what Frankling would have said which is",newCountToGuess+"!")
+}
