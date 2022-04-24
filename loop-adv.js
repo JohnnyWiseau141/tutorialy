f1a4e65b823beb4b3453f69966bdf3beed8279e7
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-04-24 19:07:07 -0400

    indentation fix on loop-adv

--- loop-adv.js
+++ loop-adv.js
@@ -7,33 +7,33 @@ var startingNumber = sbeve.inputNumber()
 
 if (startingNumber === 0) {
     console.log("You chose zero! Many fictional characters are named zero!")
-} else {
+    } else {
 
-    if ( startingNumber < 0 ) {
-        var negativeNumber
-        negativeNumber = startingNumber
+        if ( startingNumber < 0 ) {
+            var negativeNumber
+            negativeNumber = startingNumber
 
-        negativeNumber = startingNumber - negativeNumber - 1
+            negativeNumber = startingNumber - negativeNumber - 1
 
-        while (negativeNumber >= startingNumber) {
-            console.log(negativeNumber)
-            negativeNumber = negativeNumber - 1
+            while (negativeNumber >= startingNumber) {
+                console.log(negativeNumber)
+                negativeNumber = negativeNumber - 1
+            }
         }
+
+        
+        var daChangingNumber
+
+        daChangingNumber = startingNumber
+
+
+        daChangingNumber = startingNumber - daChangingNumber +1
+
+            while ( daChangingNumber <= startingNumber ) { 
+
+                console.log(daChangingNumber)
+
+                daChangingNumber = daChangingNumber + 1
+            }
+
     }
-
-    
-    var daChangingNumber
-
-    daChangingNumber = startingNumber
-
-
-    daChangingNumber = startingNumber - daChangingNumber +1
-
-    while ( daChangingNumber <= startingNumber ) { 
-
-        console.log(daChangingNumber)
-
-        daChangingNumber = daChangingNumber + 1
-}
-
-}
