7779b649fb23c8541b6b2d210f27f62b5673f68d
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-04-24 16:17:15 -0400

    starting loop advance exercise

---
+++ loop-adv.js
@@ -0,0 +1,27 @@
+const sbeve = require('./sbeve.js')
+
+console.log("Choose a number to count to:")
+
+var startingNumber = sbeve.inputNumber()
+
+if (startingNumber === 0) {
+    console.log("You chose zero! Many fictional characters are named zero!")
+} else {
+
+
+    var daChangingNumber
+    var daChangingNumber
+
+    daChangingNumber = startingNumber
+
+
+    daChangingNumber = startingNumber - daChangingNumber +1
+
+    while ( daChangingNumber <= startingNumber ) { 
+
+        console.log(daChangingNumber)
+
+        daChangingNumber = daChangingNumber + 1
+}
+
+}
