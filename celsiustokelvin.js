d0c87c9df661f1468503db38db1b4fee0dccde9d
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-04-25 10:25:13 -0400

    celsius to kelvin program basic

--- celsiustokelvin.js
+++ celsiustokelvin.js
@@ -1,2 +1,19 @@
 const sbeve = require('./sbeve.js')
 console.log("Enter a temperature in °C")
+
+var tempC
+tempC = sbeve.inputNumber()
+
+var tempK
+
+tempK = tempC + 273.15
+
+if (tempK === 273.15 ) {
+    console.log(tempK, "that's the freezing/melting point of water!")
+}
+
+if (tempK === 294.15) {
+    console.log(tempK, "that's room temperature!")
+}
+
+console.log("You converted from", tempC+"°C to", tempK+"°K")
