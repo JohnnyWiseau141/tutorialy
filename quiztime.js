2b1f9a27dc49282ad235e0792dbf3f8a156f7b47
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-04-29 14:32:12 -0400

    use while loop for first unofficial question

--- quiztime.js
+++ quiztime.js
@@ -2,6 +2,8 @@ const sbeve = require('./sbeve.js')
 
 console.log("So you think you're a cool guy, eh? Well this sure ain't Strong Bad's stick, but it's something better. A quiz to guage your coolness!")
 
+
+
 console.log("Question numero uno: Why are you here?")
 console.log("A) To win the holy grail!")
 console.log("B) To learn how to cheat!")
@@ -11,13 +13,24 @@ console.log("D) To take a quiz for cool guys, ya fool!")
 var whyAreYouHere
 whyAreYouHere = sbeve.input()
 
-if (whyAreYouHere === "a" || whyAreYouHere ==="A" ) {
-    console.log("If you wanna win the holy grail go watch Monty Python or play Fate/Stay Night, ya fool!")
-} else if (whyAreYouHere === "b" || whyAreYouHere ==="B") {
-    console.log("Only liars and thieves eat grumble cakes! Those people go to prison. Just ask the cheat!")
-} else if (whyAreYouHere === "c" || whyAreYouHere ==="C") {
-    console.log("Then give me some of them, ya fool!")
-} else if (whyAreYouHere === "d" || whyAreYouHere === "D" ) {
+while ( whyAreYouHere !== "d" && whyAreYouHere !== "D" ) {
+    if (whyAreYouHere === "a" || whyAreYouHere ==="A" ) {
+        console.log("If you wanna win the holy grail go watch Monty Python or play Fate/Stay Night, ya fool!")
+    } else if (whyAreYouHere === "b" || whyAreYouHere ==="B") {
+        console.log("Only liars and thieves eat grumble cakes! Those people go to prison. Just ask the cheat!")
+    } else if (whyAreYouHere === "c" || whyAreYouHere ==="C") {
+        console.log("Then give me some of them, ya fool!")
+    }
+    console.log("I will ask again:")
+    console.log("Question numero uno: Why are you here?")
+    console.log("A) To win the holy grail!")
+    console.log("B) To learn how to cheat!")
+    console.log("C) To show off my fun coupons!")
+    console.log("D) To take a quiz for cool guys, ya fool!")
+    whyAreYouHere = sbeve.input()
+} 
+
+if (whyAreYouHere === "d" || whyAreYouHere === "D" ) {
     console.log("Well you came to the right place! Just answer the rest of the questions and you'll be a cool guy, just like the four cool guys who drank four cool drinks!")
     console.log("You need to answering the questions correctly.")
     console.log("Failure will find you explainging why to a far less... patient audience.")
@@ -79,7 +92,7 @@ if (whyAreYouHere === "a" || whyAreYouHere ==="A" ) {
                 console.log("Kowalski! This person thinks the Penguins of Madagascar are gonna be here. Wrong answer!")
             } else if (thirdOfficialResponse === "b" || thirdOfficialResponse === "B") {
                 console.log("My face has no such relation to the USA state of Vermont, I can guarantee.")
-            } else if (thirdOfficialResponse === "D" || thirdOfficialResponse === "d") {
+            } else if (thirdOfficialResponse === "d" || thirdOfficialResponse === "D") {
                 console.log("Lake Champlain isn't even a town or city. Wrongo you are.")
             } else if (thirdOfficialResponse === "c" || thirdOfficialResponse === "C") {
                 console.log("You're correct! You know your stuff!")
@@ -98,9 +111,9 @@ if (whyAreYouHere === "a" || whyAreYouHere ==="A" ) {
                 console.log("You are not Johnny therefore you have no need for these. No go apologize and return Johnny's Cheddar fries and 3D doritos.")
             } else if (fourthOfficialResponse === "c" || fourthOfficialResponse === "C") {
                 console.log("You chose a Nintendo Switch at the end of the world? Commendable, but wrong answer.")
-            } else if (fourthOfficialResponse === "D" || fourthOfficialResponse === "d") {
+            } else if (fourthOfficialResponse === "d" || fourthOfficialResponse === "D") {
                 console.log("Wrong, Bison breath! Patrick Star says hero worship is unhealthy and I concur with him. C'mon, Jeffrey.")
-            } else if (fourthOfficialResponse === "B" || fourthOfficialResponse === "b") {
+            } else if (fourthOfficialResponse === "b" || fourthOfficialResponse === "B") {
                 console.log("You chose this over a nintendo switch subscription? You're bold, and your boldness pays off!")
 
                 console.log("More are coming. You just finished the fourth question.")
@@ -117,9 +130,9 @@ if (whyAreYouHere === "a" || whyAreYouHere ==="A" ) {
                 console.log("This isn't Captain Underpants! Such a character does not exist. Wrongo")
             } else if (fifthOfficialResponse === "b" || fifthOfficialResponse === "B") {
                 console.log("WRONG! This is the Two Towers which means they're not playable!")
-            } else if (fifthOfficialResponse === "C" || fifthOfficialResponse === "c") {
+            } else if (fifthOfficialResponse === "c" || fifthOfficialResponse === "C") {
                 console.log("Not the MCU so afraid that ain't gonna fly roung here. Wrongo ping pong answer.")
-            } else if (fifthOfficialResponse === "D" || fifthOfficialResponse === "d") {
+            } else if (fifthOfficialResponse === "d" || fifthOfficialResponse === "D") {
                 console.log("Good answer! You win the")
 
                 console.log("More are coming. You've completed question five.")
@@ -135,90 +148,90 @@ if (whyAreYouHere === "a" || whyAreYouHere ==="A" ) {
             if (sixthOfficialResponse === "d" || sixthOfficialResponse === "D") {
                 console.log("Mandy is a dangerous person so afraid that is wrong. Good thing you ain't Grim.")
             } else if (sixthOfficialResponse === "b" || sixthOfficialResponse === "B") {
-                console.log("You chose a.")
-            } else if (sixthOfficialResponse === "c" || sixthOfficialResponse === "c") {
-                console.log("wowowowow.")
+                console.log("The bird is actually the word but this question is actually made by Europeans therefore it's not innocent of something hint hint.")
+            } else if (sixthOfficialResponse === "c" || sixthOfficialResponse === "C") {
+                console.log("Nop. Too bad wrong answer.")
             } else if (sixthOfficialResponse === "a" || sixthOfficialResponse === "A") {
-                console.log("huh!")
+                console.log("Righteo, Cheerio! It is indeed dumb dumb stupid white white guilt.")
 
-                console.log("More are coming. After the sixth question, you have.")
+                console.log("More are coming. Cross the sixth question, you have.")
 
                 var seventhOfficialResponse
-                console.log('What is the capital of my face?')
-                console.log("A) your face")
-                console.log("B) his face face!")
-                console.log("C) poopy pie")
-                console.log("D) my mom")
+                console.log('How many times did Strong Bad kick in the face?')
+                console.log("A) Infinity War")
+                console.log("B) Nine times!")
+                console.log("C) Throw someone out the window meme!")
+                console.log("D) Schfifty five")
                 seventhOfficialResponse = sbeve.input()
 
             if (seventhOfficialResponse === "a" || seventhOfficialResponse === "A") {
-                console.log("You chose a!")
+                console.log("You chose a really dope movie! Too bad it's the wrong answer. Sad!")
+            } else if (seventhOfficialResponse === "c" || seventhOfficialResponse === "c") {
+                console.log("Congrats your office is liable for murder. That makes you A GIGANTIC PUNDA BUBU.")
+            } else if (seventhOfficialResponse === "d" || seventhOfficialResponse === "D") {
+                console.log("WHAT YOU SAY?! Your iq is clearly schfifty five if you think that's a numero.")
             } else if (seventhOfficialResponse === "b" || seventhOfficialResponse === "B") {
-                console.log("You chose a.")
-            } else if (seventhOfficialResponse === "D" || seventhOfficialResponse === "d") {
-                console.log("wowowowow.")
-            } else if (seventhOfficialResponse === "c" || seventhOfficialResponse === "C") {
-                console.log("huh!")
+                console.log("He can do it! He will do it 9 times.")
 
                 console.log("More are coming. You passed the seventh question.")
                         
 
                 var eighthOfficialResponse
-                console.log('What is the capital of my face?')
-                console.log("A) your face")
-                console.log("B) his face face!")
-                console.log("C) poopy pie")
-                console.log("D) my mom")
+                console.log('What does "Punda tah Punda" allegedly mean in english, according to Star Wars Episode I The Phantom Menace?')
+                console.log("A) That little human being is out of his mind!")
+                console.log("B) If a lot of people love each other, the wurld would be a better place.")
+                console.log("C) If only we had a sack of potatoes!")
+                console.log("D) You'll be dead guy! and... You mean buttface?!")
                 eighthOfficialResponse = sbeve.input()
 
-            if (eighthOfficialResponse === "a" || eighthOfficialResponse === "A") {
-                console.log("You chose a!")
+            if (eighthOfficialResponse === "c" || eighthOfficialResponse === "C") {
+                console.log("A sack of potetoes would not aid us in any way. Wrong answer")
             } else if (eighthOfficialResponse === "b" || eighthOfficialResponse === "B") {
-                console.log("You chose a.")
-            } else if (eighthOfficialResponse === "D" || eighthOfficialResponse === "d") {
-                console.log("wowowowow.")
-            } else if (eighthOfficialResponse === "c" || eighthOfficialResponse === "C") {
-                console.log("huh!")
+                console.log("Anything for my Princess. Hehehe. What a story. A wrong answer story!")
+            } else if (eighthOfficialResponse === "d" || eighthOfficialResponse === "D") {
+                console.log("You missed rogue one! That was at the beginning of this quiz! Wrong answer!")
+            } else if (eighthOfficialResponse === "a" || eighthOfficialResponse === "A") {
+                console.log("You are correct! It's exactly what the guy said in the Phantom Menace during the Pod Race!")
 
                 console.log("More are coming.You passed the eighth.")
                         
 
                             var ninthOfficialResponse
-                            console.log('What is the capital of my face?')
-                            console.log("A) your face")
-                            console.log("B) his face face!")
-                            console.log("C) poopy pie")
-                            console.log("D) my mom")
+                            console.log('How many times was Ferris Bueller truly absent?')
+                            console.log("A) 7-11")
+                            console.log("B) One hundred forty one")
+                            console.log("C) Nine times")
+                            console.log("D) Barrel Dragon's worth amount of times")
                             ninthOfficialResponse = sbeve.input()
 
                                             if (ninthOfficialResponse === "a" || ninthOfficialResponse === "A") {
                             console.log("You chose a!")
                         } else if (ninthOfficialResponse === "b" || ninthOfficialResponse === "B") {
                             console.log("You chose a.")
-                        } else if (ninthOfficialResponse === "D" || ninthOfficialResponse === "d") {
+                        } else if (ninthOfficialResponse === "d" || ninthOfficialResponse === "D") {
                             console.log("wowowowow.")
                         } else if (ninthOfficialResponse === "c" || ninthOfficialResponse === "C") {
-                            console.log("huh!")
+                            console.log("That's right! What a familiar number!")
 
-                                                console.log("One more thing. I can do it. I can do it nine times.")
+                                                console.log("One more thing. Final question. Can you do it? I can do it. I can do it nine times.")
                         
 
                                                     var tenthOfficialResponse
-                                                    console.log('What is the capital of my face?')
-                                                    console.log("A) your face")
-                                                    console.log("B) his face face!")
-                                                    console.log("C) poopy pie")
-                                                    console.log("D) my mom")
+                                                    console.log('Where did "dumb stupid white guilt" originate from?')
+                                                    console.log("A) Doc and Mahty")
+                                                    console.log("B) Rick and Morty!")
+                                                    console.log("C) Cow and Chicken")
+                                                    console.log("D) Homestar runner dot net. It's dot com.")
                                                     tenthOfficialResponse = sbeve.input()
 
                                                     if (tenthOfficialResponse === "a" || tenthOfficialResponse === "A") {
                                                         console.log("You chose a!")
-                                                    } else if (tenthOfficialResponse === "b" || tenthOfficialResponse === "B") {
+                                                    } else if (tenthOfficialResponse === "d" || tenthOfficialResponse === "D") {
                                                         console.log("You chose a.")
-                                                    } else if (tenthOfficialResponse === "D" || tenthOfficialResponse === "d") {
-                                                        console.log("wowowowow.")
                                                     } else if (tenthOfficialResponse === "c" || tenthOfficialResponse === "C") {
-                                                        console.log("huh!")
+                                                        console.log("wowowowow.")
+                                                    } else if (tenthOfficialResponse === "b" || tenthOfficialResponse === "B") {
+                                                        console.log("You did it! You got the answer right!")
                                                     
                                                         console.log("Congrats on making it to the end! You won the mile! How did what's your secret, person who is about to suddenly shout about the Cheat being a millionaire?")
 
