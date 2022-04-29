828af58fd71156cce5c8f19a35facfbb9b0d1eb4
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-04-29 11:20:22 -0400

    add third quiz question choices and responses

--- quiztime.js
+++ quiztime.js
@@ -23,7 +23,7 @@ if (whyAreYouHere === "a" || whyAreYouHere ==="A" ) {
     console.log("Now time for the first real question:")
 
     var firstRealResponse
-    console.log('Complete the quote: "Ahhh fudge it! Let\'s just talk about __')
+    console.log('Complete the quote: "Ahhh fudge it! Let\'s just talk about _"')
     console.log("A) X-WINGS!")
     console.log("B) DEATH STAR!")
     console.log("C) GRAND MOFF TARKIN!!")
@@ -44,8 +44,48 @@ if (whyAreYouHere === "a" || whyAreYouHere ==="A" ) {
 
         console.log("More are coming. After all I am a fart that poops gold.")
 
+
+        var secondOfficialResponse
+        console.log('The following are for scrubs:')
+        console.log("A) South Park, Rick and Morty after season 3, Postman, and literally anything after the first one.")
+        console.log("B) Everything including the gamecube and Nintendo!")
+        console.log("C) Opposable thumbs")
+        console.log("D) Gen 1 and 2 Pokemon")
+                secondOfficialResponse = sbeve.input()
+
+        if (secondOfficialResponse === "b" || secondOfficialResponse === "b") {
+            console.log("Nintendo saved the gaming industry so I guess you are wrongo!")
+        } else if (secondOfficialResponse === "c" || secondOfficialResponse === "C") {
+            console.log("Opposable thumbs are actually super handy and it's a big reason why humans are dope")
+        } else if (secondOfficialResponse === "d" || secondOfficialResponse === "D") {
+            console.log("Gen 1 and 2 may be for scrubs but it's also for not-scrubs so wrongo you are-o!")
+        } else if (secondOfficialResponse === "a" || secondOfficialResponse === "A") {
+            console.log("You're correct! You know your stuff!")
+
+            console.log("More are coming. After all I am a quiz that is capable of counting beyond three.")
+
+            var thirdOfficialResponse
+            console.log('What is the capital of Vermont?')
+            console.log("A) Madagascar")
+            console.log("B) Your face!")
+            console.log("C) Montpelier")
+            console.log("D) Lake Champlain")
+            thirdOfficialResponse = sbeve.input()
+
+        if (thirdOfficialResponse === "a" || thirdOfficialResponse === "A") {
+            console.log("Kowalski! This person thinks the Penguins of Madagascar are gonna be here. Wrong answer!")
+        } else if (thirdOfficialResponse === "b" || thirdOfficialResponse === "B") {
+            console.log("My face has no such relation to the USA state of Vermont, I can guarantee.")
+        } else if (thirdOfficialResponse === "D" || thirdOfficialResponse === "d") {
+            console.log("Lake Champlain isn't even a town or city. Wrongo you are.")
+        } else if (thirdOfficialResponse === "c" || thirdOfficialResponse === "C") {
+            console.log("You're correct! You know your stuff!")
+
+            console.log("More are coming. After all I am a quiz that is capable of counting beyond three.")
+        }
+        }
+
+
+        
     }
-
-
-
 }
