df2b209e454911cb445b0534616880359867fdd4
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-02 15:17:10 -0400

    last char conditional for no input

--- last-char.js
+++ last-char.js
@@ -8,4 +8,9 @@ var lastChar
 
 lastChar = daChoice[daChoice.length - 1]
 
+if (daChoice === "" ) {
+    console.log("Watch what you're typing, ya fool!")
+} else {
+
 print("The final super duper Tuguro TRUE 100% form is... "+lastChar)
+}
