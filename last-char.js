f9c378ac9b0ff4a6055160dc41634bb1a722ecb1
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-02 15:09:59 -0400

    last-char.js

---
+++ last-char.js
@@ -0,0 +1,11 @@
+const sbeve = require('./sbeve.js')
+
+print("Type something and I will tell you the FINAL LASTEST CHARACTER EVER!!!!!!!!")
+
+var daChoice = input()
+
+var lastChar
+
+lastChar = daChoice[daChoice.length - 1]
+
+print("The final super duper Tuguro TRUE 100% form is... "+lastChar)
