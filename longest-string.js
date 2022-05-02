6590cd381f95a56741dd1133a09e0a9e478de97e
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-02 14:59:36 -0400

    longest string remove cruft

--- longest-string.js
+++ longest-string.js
@@ -18,27 +18,3 @@ while (done === "nop") {
 }
 
 console.log("You longest string was "+biggestLength)
-
-
-
-// if (firstInput === 'done') {
-//     print("You didn't type anything!")
-// } else {
-
-    
-//     while (moreInput !== 'done') {
-//         whileCounter += 1
-//         if (moreInput.length >= biggestLength.length) {
-//             biggestLength = moreInput
-//         }
-        
-//         moreInput = input()
-        
-//     }
-
-//     if (whileCounter < 2 ) {
-//     print("Your biggest length was "+firstInput+"!")
-//     } else {
-//         print("Your biggest length was "+biggestLength+"!")
-//     }
-// }
