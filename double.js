7d81dbbe3497f164f53cd03eeeed642a5fb62dac
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-05 12:52:46 -0400

    double.js

---
+++ double.js
@@ -0,0 +1,20 @@
+const sbeve = require('./sbeve.js')
+
+print("Type something in and I'll double each character!")
+
+var inputInHell = input()
+
+var countey = 0
+
+var replaceDaStringyDingy = ""
+
+for (var countey = 0 ;countey < inputInHell.length; countey += 1 ) {
+
+
+    replaceDaStringyDingy += inputInHell[countey]+inputInHell[countey]
+    
+
+    
+}
+
+print("Your swapped string is: "+replaceDaStringyDingy)
