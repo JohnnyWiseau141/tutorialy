71a87d962713dba98afd27a4ed4a080f26414d66
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-09 15:55:44 -0400

    stub up chop 10

---
+++ chop-10.js
@@ -0,0 +1,9 @@
+const sbeve = require('./sbeve.js')
+
+print("Type something and da butcher over there is gonna chop off 10 pieces. Crazy person.")
+
+var inputUrFace = input()
+
+for (var index = 0; index < inputUrFace.length; index += 1) {
+    print('your face')
+}
