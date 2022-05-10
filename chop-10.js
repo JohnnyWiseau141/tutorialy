cb923fe3d34ca68a58b9c104b6bc83e30dcca993
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-10 10:51:37 -0400

    chop-10.js

--- chop-10.js
+++ chop-10.js
@@ -1,9 +1,23 @@
 const sbeve = require('./sbeve.js')
 
-print("Type something and da butcher over there is gonna chop off 10 pieces. Crazy person.")
+print("Type something and da butcher over there is gonna chop off the first 10 pieces of whatever you chop. Crazy person.")
 
 var inputUrFace = input()
 
-for (var index = 0; index < inputUrFace.length; index += 1) {
-    print('your face')
+var dumNewString = ""
+
+var outputUrFace
+
+if (inputUrFace.length < 10 ) {
+    print("That's not enugg ya fool!")
+} else if ( inputUrFace.length === 10 ) {
+    print("Now your string is exactly empty! Look what you do, ya fool!")
+} else {
+
+    for (var index = 10; index < inputUrFace.length; index += 1) {
+        dumNewString += inputUrFace[index]
+    }
+    
+    print("Your butchered string: "+dumNewString)
 }
+
