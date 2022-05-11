e6b42cfcbccfb3ff1e7e47ef235a8a96dd972c0e
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-11 10:54:32 -0400

    yay functions hello wurld tommy wiseau has arrived

---
+++ twice.js
@@ -0,0 +1,14 @@
+// functions times mother fudgers
+
+//make a function that invokes and says hello wurld or something similar twice!
+
+const sbeve = require('./sbeve.js')
+
+
+function tommyWiseau() {
+    print("I'M FED UP WITH THIS WURLD! EVERYONE BETRAY ME!")
+}
+
+
+tommyWiseau()
+tommyWiseau()
