c711916c1c0f97594b3a0570abed8e0e1a055254
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-11 11:03:37 -0400

    can you make your prompt-twice.js file a little... better?

--- prompt-twice.js
+++ prompt-twice.js
@@ -1,20 +1,20 @@
 //make sure it prompts for your name then makes a greeting... TWICE!
 
+// OOPS I FORGOT TO INCLUDE INPUT AS PART OF THE FUNCTION OH NOOOOOOOOOOOOOOOOOOO
+
 const sbeve = require('./sbeve.js')
 
-print("Tell me your name or the FBI is gonna show up! FBI OPEN UP!")
 
-dumName = input()
+
+
 
 function suchGreeting() {
+    print("Tell me your name or the FBI is gonna show up! FBI OPEN UP!")
+    dumName = input()
     print("Hello there, "+dumName+"! The FBI is waiting for you!")
 }
 
 suchGreeting()
-
-print("Tell me your name or the FBI is gonna show up! FBI OPEN UP!")
-
-dumName = input()
 
 
 suchGreeting()
