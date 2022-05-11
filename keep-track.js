effb5ab5793395d9fd342675ddb245eb8fd11356
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-11 11:20:02 -0400

    keep-track.js

---
+++ keep-track.js
@@ -0,0 +1,21 @@
+// Write a function that keeps track of how many times it's been called, and prints it. Call it three times
+//What's this johnny is using a variable that only consists of ONE CHARACTER?????!!!!!?!?!?!?!?!
+const sbeve = require('./sbeve.js')
+
+var x = 1
+
+function meesaCallTracks () {
+
+    if (x ===1 ) {
+        print("I've been called "+x+" time!")
+    } else {
+
+    print("I've been called "+x+" times!")
+    }
+
+    x += 1
+}
+
+meesaCallTracks()
+meesaCallTracks()
+meesaCallTracks()
