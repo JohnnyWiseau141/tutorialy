1b9382c1aff167c58d5abc53024109d1cd86b9f1
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-13 17:21:11 -0400

    better conditionals for aa to codon translation

--- codon-aa-translation.js
+++ codon-aa-translation.js
@@ -6,28 +6,52 @@ print("Enter an amino acid, START, or STOP and I will tell you it's three-letter
 
 var aminoAcidInput = input()
 
+var goodInput
 
 for (var index = 0; index < codons.length; index+=1) {
     
-    //Conditional for stop codons
+    //Conditional for stop
     if (aminoAcidInput === "STOP" && aminoAcidInput === codons[index][0] ) {
         print("Stop codons are "+codons[index][1]+", "+codons[index][2]+", and "+codons[index][3])
         break
-    //conditional for start
-    } else if (aminoAcidInput === "START" && index === 1 ) {
-        print("That's "+codons[index][0]+", which is also a START codon consisting of "+codons[index][2])
+    //conditional for start and methionine which is also a start codon
+     } else if ( aminoAcidInput === "START" 
+                && index === 1
+                ||aminoAcidInput === "methionine" 
+                && aminoAcidInput === codons[index][0] 
+                || aminoAcidInput === "met"
+                && aminoAcidInput === codons[index][1]) {
+        print("That's "+codons[index][0]+", which is also a START codon made of "+codons[index][2])
         break
-    // conditional for methionine which is also a start codon
-    } else if (aminoAcidInput === "methionine" && aminoAcidInput === codons[index][0]) {
-        print("That's "+codons[index][0]+", which is also a START codon consisting of "+codons[index][2])
-        break
-    //conditional for methionione when put in abbreviated
-    } else if (aminoAcidInput === "met" && aminoAcidInput === codons[index][1]) {
-        print("That's "+codons[index][0]+", a START codon consisting of "+codons[index][2])
-        break
-    } else if (codons[index].length === 4 && codons[index][0] !== "STOP" && aminoAcidInput === codons[index][0]) {    
+    // normal ish conditionals finally
+    } else if (codons[index].length === 4 
+                && codons[index][0] !== "STOP" 
+                && aminoAcidInput === codons[index][0]
+                || aminoAcidInput === codons[index][1]
+                && codons[index].length === 4 
+                && codons[index][0] !== "STOP") {    
+
         print("You entered "+codons[index][0]+" which is abbreviated as " +codons[index][1]+ " and made up of codons "+codons[index][2]+ " and " +codons[index][3] )
-    } else if (aminoAcidInput === codons[index][0] && codons[index].length === 5 || aminoAcidInput === codons[index][1] && codons[index].length === 5 ) {
+        break
+    } else if (aminoAcidInput === codons[index][0] 
+                && codons[index].length === 5 
+                || aminoAcidInput === codons[index][1] 
+                && codons[index].length === 5 ) {
         print("You entered "+codons[index][0]+" which is abbreviated as " +codons[index][1]+ " and made up of codons "+codons[index][2]+", " +codons[index][3]+", and "+codons[index][4] )
+        break
+    } else if (aminoAcidInput === codons[index][0] 
+                && codons[index].length === 6 
+                || aminoAcidInput === codons[index][1] 
+                && codons[index].length === 6) {
+            print("You entered "+codons[index][0]+" which is abbreviated as " +codons[index][1]+ " and made up of codons "+codons[index][2]+", " +codons[index][3]+", "+codons[index][4]+", and "+codons[index][5] )
+            break
+    } else if (aminoAcidInput === codons[index][0] 
+                && codons[index].length === 8 
+                || aminoAcidInput === codons[index][1] 
+                && codons[index].length === 8) {
+            print("You entered "+codons[index][0]+" which is abbreviated as " +codons[index][1]+ " and made up of codons "+codons[index][2]+", " +codons[index][3]+", "+codons[index][4]+", "+codons[index][5]+", "+codons[index][6]+", and "+codons[index][7] )
+            break
+    } else {
+        goodInput = false
     }
 }
