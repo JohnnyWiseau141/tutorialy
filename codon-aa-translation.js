618143764f459b71a8efc466490946d89e7ca739
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-05-16 11:21:01 -0400

    allow for codon input into aa-codon program

--- codon-aa-translation.js
+++ codon-aa-translation.js
@@ -5,15 +5,43 @@ var codons = require('./codon-aa-array.js')
 print("Enter an amino acid, START, or STOP and I will tell you it's three-letter mRNA codon, or codons!")
 
 var aminoAcidInput = input()
+
+if (aminoAcidInput.length === 3 
+     && aminoAcidInput !== "met"
+     && aminoAcidInput !== "phe" 
+     && aminoAcidInput !== "ile" 
+     && aminoAcidInput !== "pro" 
+     && aminoAcidInput !== "leu" 
+     && aminoAcidInput !== "arg" 
+     && aminoAcidInput !== "ala" 
+     && aminoAcidInput !== "asn" 
+     && aminoAcidInput !== "asp" 
+     && aminoAcidInput !== "val" 
+     && aminoAcidInput !== "ser" 
+     && aminoAcidInput !== "thr"
+     && aminoAcidInput !== "tyr"
+     && aminoAcidInput !== "his"
+     && aminoAcidInput !== "gln"
+     && aminoAcidInput !== "lys"
+     && aminoAcidInput !== "glu"
+     && aminoAcidInput !== "cys"
+     && aminoAcidInput !== "trp"
+     && aminoAcidInput !== "gly") {
+        aminoAcidInput = aminoAcidInput.toUpperCase()
+    } else {
     aminoAcidInput = aminoAcidInput.toLowerCase()
+    }
 
 var goodInput
 
 for (var index = 0; index < codons.length; index+=1) {
     goodInput = true
     //Conditional for stop
-    if (aminoAcidInput === "stop" && aminoAcidInput === codons[index][0] ) {
-        print("Stop codons are "+codons[index][1]+", "+codons[index][2]+", and "+codons[index][3])
+    if (aminoAcidInput === "stop" && aminoAcidInput === codons[index][0] 
+        || aminoAcidInput === codons[index][1] && aminoAcidInput === "UAA"
+        || aminoAcidInput === codons[index][2] && aminoAcidInput === "UAG"
+        || aminoAcidInput === codons[index][3] && aminoAcidInput === "UGA") {
+                print("Stop codons are "+codons[index][1]+", "+codons[index][2]+", and "+codons[index][3])
         goodInput = true
         break
     //conditional for start and methionine which is also a start codon
@@ -22,7 +50,9 @@ for (var index = 0; index < codons.length; index+=1) {
                 ||aminoAcidInput === "methionine" 
                 && aminoAcidInput === codons[index][0] 
                 || aminoAcidInput === "met"
-                && aminoAcidInput === codons[index][1]) {
+                && aminoAcidInput === codons[index][1]
+                || aminoAcidInput === "AUG"
+                && aminoAcidInput === codons[index][2]) {
         print("That's "+codons[index][0]+", which is also a START codon made of "+codons[index][2])
         break
     // normal ish conditionals finally
@@ -31,7 +61,8 @@ for (var index = 0; index < codons.length; index+=1) {
                 && aminoAcidInput === codons[index][0]
                 || aminoAcidInput === codons[index][1]
                 && codons[index].length === 3 
-                && codons[index][1] !== "met") {    
+                && codons[index][1] !== "met"
+                || aminoAcidInput === codons[index][2]) {    
                 
         print("You entered "+codons[index][0]+" which is abbreviated as " +codons[index][1]+ " and made up of codon "+codons[index][2])
         break
@@ -47,19 +78,32 @@ for (var index = 0; index < codons.length; index+=1) {
     } else if (aminoAcidInput === codons[index][0] 
                 && codons[index].length === 5 
                 || aminoAcidInput === codons[index][1] 
-                && codons[index].length === 5 ) {
+                && codons[index].length === 5 
+                || aminoAcidInput === codons[index][2]
+                || aminoAcidInput === codons[index][3]
+                || aminoAcidInput === codons[index][4]) {
         print("You entered "+codons[index][0]+" which is abbreviated as " +codons[index][1]+ " and made up of codons "+codons[index][2]+", " +codons[index][3]+", and "+codons[index][4] )
         break
     } else if (aminoAcidInput === codons[index][0] 
                 && codons[index].length === 6 
                 || aminoAcidInput === codons[index][1] 
-                && codons[index].length === 6) {
+                && codons[index].length === 6
+                || aminoAcidInput === codons[index][2]
+                || aminoAcidInput === codons[index][3]
+                || aminoAcidInput === codons[index][4]
+                || aminoAcidInput === codons[index][5]) {
             print("You entered "+codons[index][0]+" which is abbreviated as " +codons[index][1]+ " and made up of codons "+codons[index][2]+", " +codons[index][3]+", "+codons[index][4]+", and "+codons[index][5] )
             break
     } else if (aminoAcidInput === codons[index][0] 
                 && codons[index].length === 8 
                 || aminoAcidInput === codons[index][1] 
-                && codons[index].length === 8) {
+                && codons[index].length === 8
+                || aminoAcidInput === codons[index][2]
+                || aminoAcidInput === codons[index][3]
+                || aminoAcidInput === codons[index][4]
+                || aminoAcidInput === codons[index][5]
+                || aminoAcidInput === codons[index][6]
+                || aminoAcidInput === codons[index][7]) {
             print("You entered "+codons[index][0]+" which is abbreviated as " +codons[index][1]+ " and made up of codons "+codons[index][2]+", " +codons[index][3]+", "+codons[index][4]+", "+codons[index][5]+", "+codons[index][6]+", and "+codons[index][7] )
             break
     } else {
@@ -69,5 +113,6 @@ for (var index = 0; index < codons.length; index+=1) {
 }
 
 if (!goodInput) {
-    print("Shoulda entered a valid AA or simple STOP or START string. Woe is you.")
+    print("You entered "+aminoAcidInput)
+    print("I guess you shoulda entered a valid AA or simple STOP or START string. Woe is you.")
 }
