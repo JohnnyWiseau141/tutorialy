f532cadb60c19fd49e1c92921197507e311519d2
JohnnyWiseau141 <john.a.foley141@gmail.com>
2022-07-06 12:15:54 -0400

    changing guessing-game.js to for loop

--- guessing-game.js
+++ guessing-game.js
@@ -1,22 +1,37 @@
 const sbeve = require('./sbeve.js')
 
-var winnerIsU
-winnerIsU = 66
+
 
 console.log("I'm thinking of a number from 1 to 100. Take a guess and I'll let you know if your guess should be higher or lower!")
-guessedNumber = sbeve.inputNumber()
+let guessedNumber = sbeve.inputNumber()
+daCounter = 0
 
-while (guessedNumber !== winnerIsU) {
-    
-    if (guessedNumber > winnerIsU) {
-        console.log("Lower")
+for (let daAnswer = 66; guessedNumber !== daAnswer; daCounter++) {
 
-    } else if (guessedNumber < winnerIsU ) {
-        console.log("Higher")
-
-    }
+    if (guessedNumber > daAnswer) {
+                console.log("Lower")
+            } else if (guessedNumber < daAnswer ) {
+                console.log("Higher")
+            }
     guessedNumber = sbeve.inputNumber()
-
 }
 
 console.log("Congratulations! You won the guessing game! How did what's your secret?")
+
+// var winnerIsU
+// winnerIsU = 66
+
+// while (guessedNumber !== winnerIsU) {
+    
+//     if (guessedNumber > winnerIsU) {
+//         console.log("Lower")
+
+//     } else if (guessedNumber < winnerIsU ) {
+//         console.log("Higher")
+
+//     }
+//     guessedNumber = sbeve.inputNumber()
+
+// }
+
+// console.log("Congratulations! You won the guessing game! How did what's your secret?")
